b443d2da4322b72cd118f1071208c24ddfa31534
samtube405 <sam2010ucsc@gmail.com>
2017-06-24 00:49:16 -0400

    New travis test folder, update to snap local installation

--- install-snap-python.sh
+++ install-snap-python.sh
@@ -3,3 +3,7 @@ wget https://snap.stanford.edu/snappy/release/snap-3.0.2-3.0-centos6.5-x64-py2.6
 tar zxvf snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
 cd snap-3.0.2-3.0-centos6.5-x64-py2.6
 sudo python setup.py install
+cp snap.py ../bellydynamic-travis/
+cp _snap.so ../bellydynamic-travis/
+chmod +x ../bellydynamic-travis/snap.py
+chmod +x ../bellydynamic-travis/_snap.so
