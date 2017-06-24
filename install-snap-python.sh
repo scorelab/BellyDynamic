ee8c8da735fb1ba557667c8fe42f948bfb6ea5f2
samtube405 <sam2010ucsc@gmail.com>
2017-06-24 00:20:11 -0400

    Remove sudo exec

--- install-snap-python.sh
+++ install-snap-python.sh
@@ -2,4 +2,4 @@
 wget https://snap.stanford.edu/snappy/release/snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
 tar zxvf snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
 cd snap-3.0.2-3.0-centos6.5-x64-py2.6
-sudo python setup.py install
+python setup.py install
