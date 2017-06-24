72ac64fe4965bba885ce8aae9184f93b41124c5d
samtube405 <sam2010ucsc@gmail.com>
2017-06-24 00:17:39 -0400

    Fix installation script

--- install-snap-python.sh
+++ install-snap-python.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget https://snap.stanford.edu/snappy/release/snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
-tar zxvf snap-3.0.2-3.0-centos6.5-x64-py2.6
+tar zxvf snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
 cd snap-3.0.2-3.0-centos6.5-x64-py2.6
 sudo python setup.py install
