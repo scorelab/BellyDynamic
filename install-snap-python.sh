6450794047305569ff80bdd6c2c52bf3ecfa59ed
samtube405 <sam2010ucsc@gmail.com>
2017-08-21 17:32:56 -0400

    Update snap latest release

--- install-snap-python.sh
+++ install-snap-python.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-wget https://snap.stanford.edu/snappy/release/snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
-tar zxvf snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
-cd snap-3.0.2-3.0-centos6.5-x64-py2.6
+wget https://snap.stanford.edu/snappy/release/snap-4.0.0-4.0-centos6.5-x64-py2.6.tar.gz
+tar zxvf snap-4.0.0-4.0-centos6.5-x64-py2.6.tar.gz
+cd snap-4.0.0-4.0-centos6.5-x64-py2.6.tar.gz
 sudo python setup.py install
 cp snap.py ../bellydynamic-travis/
 cp _snap.so ../bellydynamic-travis/
