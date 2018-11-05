cc7ece00f1fb65028709b635d764d81ab0b3e927
Jakub Mularski <jakubmularski723@gmail.com>
2018-11-05 18:27:35 +0100

    fix(snap): changed snap version in setup.sh and install-snap-python.sh

--- install-snap-python.sh
+++ install-snap-python.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-wget https://snap.stanford.edu/snappy/release/snap-4.0.0-4.0-centos6.5-x64-py2.6.tar.gz
-tar zxvf snap-4.0.0-4.0-centos6.5-x64-py2.6.tar.gz
-cd snap-4.0.0-4.0-centos6.5-x64-py2.6
+wget https://snap.stanford.edu/snappy/release/snap-4.1.0-4.1-centos6.5-x64-py2.6.tar.gz
+tar zxvf snap-4.1.0-4.1-centos6.5-x64-py2.6.tar.gz
+cd snap-4.1.0-4.1-centos6.5-x64-py2.6
 sudo python setup.py install
 cp snap.py ../bellydynamic-travis/
 cp _snap.so ../bellydynamic-travis/
